250baf790c19322225c3ce412ac6998efb4fc408
ElTerrorDeLasLelas <legitimoanonimo7@gmail.com>
2025-07-04 17:19:10 -0400

    aws

--- FastApipythonpv/app/main.py
+++ FastApipythonpv/app/main.py
@@ -10,3 +10,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+// Cargar todos los registros al montar el componente 
+  // Cargar los aws 
